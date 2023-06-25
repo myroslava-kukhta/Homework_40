af6612e321a6e6b3453097aad8636ef7bc69c0cb
Myroslava Kukhta <107687502+myroslava-kukhta@users.noreply.github.com>
2023-06-25 12:22:22 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import Nav from './components/nav';
+import Main from './components/main';
+
+
+function App() {
+  return (
+    <div className='app-wrapper'> 
+      <Header />
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+export default App;
---
+++ components/nav.jsx
@@ -0,0 +1,18 @@
+const Nav = () => {
+  return (
+      <nav className='nav'>
+        <div>
+          Profile
+        </div>
+        <div>
+          Messages
+        </div>
+        <div>
+          News
+        </div>
+        <div>Settings</div>
+      </nav>
+  )
+}
+
+export default Nav;
